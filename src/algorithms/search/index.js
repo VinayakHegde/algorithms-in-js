1471eeb2141d2314b72ebf41c8b004854beca37f
Vinayak Hegde <vinayak.nandi@gmail.com>
2020-05-04 23:10:08 +0100

    Jump Search exposed via index

--- src/algorithms/search/index.js
+++ src/algorithms/search/index.js
@@ -1,7 +1,9 @@
 import { BinarySearch } from './binary-search';
 import { InterpolationSearch } from './interpolation-search';
+import { JumpSearch } from './jump-search';
 
 export default {
   binarySearch: BinarySearch,
   interpolationSearch: InterpolationSearch,
+  jumpSearch: JumpSearch,
 };
