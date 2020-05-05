7b2ed02690bff7db1423c870cb8e86f44c3cdd79
Vinayak Hegde <vinayak.nandi@gmail.com>
2020-05-05 23:40:47 +0100

    Update interpolation search for number arrays and number values only - now calls recurssively

--- src/algorithms/search/interpolation-search/index.js
+++ src/algorithms/search/interpolation-search/index.js
@@ -1,32 +1,31 @@
-export const InterpolationSearch = ({ list, value }) => {
-  let start = 0;
-  let end = list.length - 1;
+export const InterpolationSearch = props => {
+  if (!props) return -1;
+  const { list, value, start, end } = props;
+  if (list === undefined || !list.length || value === undefined) return -1;
 
-  while (start <= end) {
-    const valDiff = list[end] - list[start];
-    if (!valDiff) {
-      return list[start] === value ? start : -1;
-    }
-
-    const delta = value - list[start];
-    if (delta < 0) {
-      return -1;
-    }
-
-    const indexDelta = end - start;
-    const index = start + Math.floor(delta * indexDelta / valDiff);
-
-    if (list[index] === value) {
-      return index;
-    }
-
-    if (list[index] < value) {
-      start = index + 1;
-    } else {
-      end = index - 1;
-    }
+  if (list.length === 1) {
+    if (list[0] === value) return 0;
+    return -1;
   }
+  if (list.reduce((acc, cur) => {
+    if (typeof cur === 'number') acc += 1;
+    return acc;
+  }, (typeof value === 'number') ? 0 : -1) !== list.length) return -1;
 
+  let low = start || 0;
+  if (list[low] === value) return low;
+
+  let high = end || list.length - 1;
+  if (list[high] === value) return high;
+
+  if (low <= high && value >= list[low] && value <= list[high]) {
+    const delta = (value - list[low]) / (list[high] - list[low]);
+    const position = low + Math.floor((high - low) * delta);
+    if (list[position] === value) return position;
+    if (list[position] < value) low = position + 1;
+    else high = position - 1;
+    return InterpolationSearch({ list, value, start: low, end: high });
+  }
   return -1;
 };
 
