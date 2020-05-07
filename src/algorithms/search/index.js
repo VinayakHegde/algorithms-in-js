e326e967f3d307dcb4019121e985cec4e35de2b9
Vinayak Hegde <vinayak.nandi@gmail.com>
2020-05-08 00:36:49 +0100

    Exponential Search exposed via index

--- src/algorithms/search/index.js
+++ src/algorithms/search/index.js
@@ -1,10 +1,12 @@
 import { BinarySearch } from './binary-search';
+import { ExponentialSearch } from './exponential-search';
 import { InterpolationSearch } from './interpolation-search';
 import { JumpSearch } from './jump-search';
 import { LinearSearch } from './linear-search';
 
 export default {
   binarySearch: BinarySearch,
+  exponentialSearch: ExponentialSearch,
   interpolationSearch: InterpolationSearch,
   jumpSearch: JumpSearch,
   linearSearch: LinearSearch,
