8086d5acb76f7850e5083cffc943cef269d6b05b
Vinayak Hegde <vinayak.nandi@gmail.com>
2020-05-08 22:42:02 +0100

    Exposed Fibonacci Search via index

--- src/algorithms/search/index.js
+++ src/algorithms/search/index.js
@@ -1,5 +1,6 @@
 import { BinarySearch } from './binary-search';
 import { ExponentialSearch } from './exponential-search';
+import { FibonacciSearch } from './fibonacci-search';
 import { InterpolationSearch } from './interpolation-search';
 import { JumpSearch } from './jump-search';
 import { LinearSearch } from './linear-search';
@@ -7,6 +8,7 @@ import { LinearSearch } from './linear-search';
 export default {
   binarySearch: BinarySearch,
   exponentialSearch: ExponentialSearch,
+  fibonacciSearch: FibonacciSearch,
   interpolationSearch: InterpolationSearch,
   jumpSearch: JumpSearch,
   linearSearch: LinearSearch,
