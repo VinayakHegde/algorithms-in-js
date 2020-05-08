c9572c9f4c6b6c9001c1a798bc30d92129454e66
Vinayak Hegde <vinayak.nandi@gmail.com>
2020-05-09 00:37:07 +0100

    Common tests runs via Test Suite for Exponential Search

--- src/algorithms/search/exponential-search/spec.js
+++ src/algorithms/search/exponential-search/spec.js
@@ -1,32 +1,15 @@
 import search from '.';
+import TestSuite from '../../utils/test-suite';
 
 describe('Exponential Search', () => {
-  it('should search number in sorted array', () => {
-    expect(search()).toBe(-1);
-    expect(search({ list: [1, 3] })).toBe(-1);
-    expect(search({ value: 1 })).toBe(-1);
-    expect(search({ list: [], value: 1 })).toBe(-1);
-    expect(search({ list: [1], value: 1 })).toBe(0);
-    expect(search({ list: [1, 2], value: 1 })).toBe(0);
-    expect(search({ list: [1, 2], value: 2 })).toBe(1);
-    expect(search({ list: [1, 5, 10, 12], value: 1 })).toBe(0);
-    expect(search({ list: [1, 5, 10, 12, 14, 17, 22, 100], value: 17 })).toBe(5);
-    expect(search({ list: [1, 5, 10, 12, 14, 17, 22, 100], value: 1 })).toBe(0);
-    expect(search({ list: [1, 5, 10, 12, 14, 17, 22, 100], value: 100 })).toBe(7);
-    expect(search({ list: [1, 5, 10, 12, 14, 17, 22, 100], value: 0 })).toBe(-1);
+  const testSuite = new TestSuite(search);
+  testSuite.common();
+
+  it('should return the index for non-sorted array of numbers', () => {
+    expect(search({ list: [10, 0, 11, 15, 13, 12, 14], value: 0 })).toBe(1);
   });
-
-  it('should search object in sorted array', () => {
-    const list = [
-      { number: 1, string: 'string 1', object: { number: 1 } },
-      { number: 2, string: 'string 2', object: { number: 2 } },
-      { number: 3, string: 'string 3', object: { number: 3 } },
-    ];
-
-    expect(search({ list: [], value: { number: 1 } })).toBe(-1);
-    expect(search({ list, value: { number: 4 } })).toBe(-1);
-    expect(search({ list: [list[0]], value: { number: 1 } })).toBe(0);
-    expect(search({ list, value: { number: 2 } })).toBe(1);
-    expect(search({ list, value: { number: 3 } })).toBe(2);
+  it('should return first found index in array of duplicate numbers', () => {
+    expect(search({ list: [1, 1], value: 1 })).toBe(0);
+    expect(search({ list: [1, 6, 7, 8, 12, 13, 14, 19, 21, 23, 24, 24, 24, 300], value: 24 })).toBe(10);
   });
 });
