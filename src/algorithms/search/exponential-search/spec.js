369b5662ca8273c394b0806617d59c8afd833f0c
Vinayak Hegde <vinayak.nandi@gmail.com>
2020-05-10 00:13:00 +0100

    Unit tests updated to use the test-suite

--- src/algorithms/search/exponential-search/spec.js
+++ src/algorithms/search/exponential-search/spec.js
@@ -2,14 +2,8 @@ import search from '.';
 import TestSuite from '../../utils/test-suite';
 
 describe('Exponential Search', () => {
-  const testSuite = new TestSuite(search);
-  testSuite.common();
-
-  it('should return the index for non-sorted array of numbers', () => {
-    expect(search({ list: [10, 0, 11, 15, 13, 12, 14], value: 0 })).toBe(1);
-  });
-  it('should return first found index in array of duplicate numbers', () => {
-    expect(search({ list: [1, 1], value: 1 })).toBe(0);
-    expect(search({ list: [1, 6, 7, 8, 12, 13, 14, 19, 21, 23, 24, 24, 24, 300], value: 24 })).toBe(10);
-  });
+  const test = new TestSuite(search);
+  test.common();
+  test.special.unsortedListFound();
+  test.special.firstFoundValueIndex();
 });
