993de7ba2d806df260a6564c759d36bf38696d58
Vinayak Hegde <vinayak.nandi@gmail.com>
2020-05-10 00:11:47 +0100

    Search index now exposes the defult export of the search algorithms

--- src/algorithms/search/index.js
+++ src/algorithms/search/index.js
@@ -1,15 +1,15 @@
-import { BinarySearch } from './binary-search';
-import { ExponentialSearch } from './exponential-search';
-import { FibonacciSearch } from './fibonacci-search';
-import { InterpolationSearch } from './interpolation-search';
-import { JumpSearch } from './jump-search';
-import { LinearSearch } from './linear-search';
+import BinarySearch from './binary-search';
+import ExponentialSearch from './exponential-search';
+import FibonacciSearch from './fibonacci-search';
+import InterpolationSearch from './interpolation-search';
+import JumpSearch from './jump-search';
+import LinearSearch from './linear-search';
 
 export default {
-  binarySearch: BinarySearch,
-  exponentialSearch: ExponentialSearch,
-  fibonacciSearch: FibonacciSearch,
-  interpolationSearch: InterpolationSearch,
-  jumpSearch: JumpSearch,
-  linearSearch: LinearSearch,
+  BinarySearch,
+  ExponentialSearch,
+  FibonacciSearch,
+  InterpolationSearch,
+  JumpSearch,
+  LinearSearch,
 };
