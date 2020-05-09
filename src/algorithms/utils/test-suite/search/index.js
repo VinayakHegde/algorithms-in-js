8ba327737b74bf420078727cbf47706d0a0f4a55
Vinayak Hegde <vinayak.nandi@gmail.com>
2020-05-10 00:07:14 +0100

    Test-suite aded few more special tests

--- src/algorithms/utils/test-suite/search/index.js
+++ src/algorithms/utils/test-suite/search/index.js
@@ -1,7 +1,12 @@
 import CommonSearch from './common';
 import CommonSearchObject from './object';
 import CommonSearchDefault from './default';
-import { FirstFoundValueIndex, UnsortedListNotFound } from './special';
+import {
+  FirstFoundValueIndex,
+  LastFoundValueIndex,
+  UnsortedListNotFound,
+  UnsortedListFound,
+} from './special';
 
 export default ctx => {
   ctx.common = ctx.direct(CommonSearch);
@@ -10,5 +15,7 @@ export default ctx => {
   ctx.special = {
     firstFoundValueIndex: ctx.direct(FirstFoundValueIndex),
     unsortedListNotFound: ctx.direct(UnsortedListNotFound),
+    unsortedListFound: ctx.direct(UnsortedListFound),
+    lastFoundValueIndex: ctx.direct(LastFoundValueIndex),
   };
 };
--- src/algorithms/utils/test-suite/search/special.js
+++ src/algorithms/utils/test-suite/search/special.js
@@ -4,10 +4,23 @@ export const UnsortedListNotFound = search => {
   });
 };
 
+export const UnsortedListFound = search => {
+  it('should return the index for non-sorted array of numbers', () => {
+    expect(search({ list: [10, 0, 11, 15, 13, 12, 14], value: 0 })).toBe(1);
+  });
+};
+
 export const FirstFoundValueIndex = search => {
   it('should return first found index in array of duplicate numbers', () => {
     expect(search({ list: [1, 1], value: 1 })).toBe(0);
     expect(search({ list: [1, 6, 7, 8, 12, 13, 14, 19, 21, 23, 24, 24, 24, 300], value: 24 })).toBe(10);
+  });
+};
+
+export const LastFoundValueIndex = search => {
+  it('should return first found index in array of duplicate numbers', () => {
+    expect(search({ list: [1, 1], value: 1 })).toBe(1);
+    expect(search({ list: [1, 6, 7, 8, 12, 13, 14, 19, 21, 23, 24, 24, 24, 300], value: 24 })).toBe(12);
   });
 };
 
