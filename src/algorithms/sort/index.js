8a4885cbb5d9817b8a2b5c3da16be2e5870c63a1
Vinayak Hegde <vinayak.nandi@gmail.com>
2020-05-11 22:48:59 +0100

    Sort index to export the sort classes

--- src/algorithms/sort/index.js
+++ src/algorithms/sort/index.js
@@ -1,7 +1,7 @@
-import { MergeSort } from './merge-sort';
-import { QuickSort } from './quick-sort';
+import MergeSort from './merge-sort';
+import QuickSort from './quick-sort';
 
 export default {
-  mergeSort: MergeSort,
-  quickSort: QuickSort,
+  MergeSort,
+  QuickSort,
 };
