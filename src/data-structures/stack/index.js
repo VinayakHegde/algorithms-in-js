7093016b4707766b5f8c829d69473fe840dec616
Vinayak Hegde <vinayak.nandi@gmail.com>
2020-05-12 23:48:00 +0100

    Implemented Stack data-structure

---
+++ src/data-structures/stack/index.js
@@ -0,0 +1,63 @@
+class Stack {
+  constructor() {
+    this.container = [];
+    this.length = 0;
+  }
+
+  push(element) {
+    this.container[this.length] = element;
+    this.length += 1;
+    return this;
+  }
+
+  peek() {
+    return this.container[this.length - 1];
+  }
+
+  isEmpty() {
+    return this.length === 0;
+  }
+
+  clear() {
+    this.length = 0;
+    this.container = [];
+    return this;
+  }
+
+  pop() {
+    if (this.isEmpty()) {
+      return null;
+    }
+
+    this.length -= 1;
+    return this.container.pop();
+  }
+
+  popOrder() {
+    let index = this.length - 1;
+    const elements = [];
+    while (index >= 0) {
+      elements.push(this.container[index]);
+      index--;
+    }
+
+    return elements;
+  }
+
+  popOrderToString() {
+    return this.popOrder().join(', ');
+  }
+
+  pushOrder() {
+    if (this.isEmpty()) {
+      return [];
+    }
+    return this.container;
+  }
+
+  pushOrderToString() {
+    return this.pushOrder().join(', ');
+  }
+}
+
+export default Stack;
