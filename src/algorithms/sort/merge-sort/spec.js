5b4ef6b44c8a00458b489cb45a42a2614e64ead1
Vinayak Hegde <vinayak.nandi@gmail.com>
2020-05-12 20:52:49 +0100

    Test case updated for merge sort

--- src/algorithms/sort/merge-sort/spec.js
+++ src/algorithms/sort/merge-sort/spec.js
@@ -4,7 +4,9 @@ import TestSuite from '../../utils/test-suite';
 describe('Merge Sort', () => {
   const test = new TestSuite(Sort);
   test.common();
+  test.stringSort();
   test.special.readySortedList();
   test.special.sameItemList();
+  test.special.duplicateItemsListSort();
   test.runner(mergeSort);
 });
